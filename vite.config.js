224cc95ff01b2f8048ea7c2a15439a26cfa1766b
amira <choukiamira77@gmail.com>
2024-09-25 21:18:37 +0100

    Deploying dist files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://choukiamira.github.io/todolist/',   // Chemin relatif au dépôt GitHub Pages
   plugins: [react()],
+  build: {
+    outDir: 'dist'    // Assurez-vous que 'dist' est le dossier de build
+  }
 })
